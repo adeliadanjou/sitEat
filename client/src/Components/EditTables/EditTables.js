cbd261bebcaec4ef16d8f9f6484eead8ea2b021e
Adelia d Anjou <princessadelia@gmail.com>
2018-12-18 12:19:49 +0100

    funciona tanto filas como columnas

--- client/src/Components/EditTables/EditTables.js
+++ client/src/Components/EditTables/EditTables.js
@@ -5,21 +5,32 @@ import "./EditTables.css";
 export default class EditTables extends Component {
   constructor(props) {
     super(props);
-    this.state = { value: undefined, arrayRows: [] };
+    this.state = { valueRow: undefined,valueCol: undefined, arrayRows: [] };
   }
-  handleChange = event => {
-    const { value } = event.target;
-    this.setState({ ...this.state, value: value });
-    //aqui arriba ya tengo el valor del input range
-  };
+
+  //le paso el isColoRow y lo comparo con el r y el c que le puse antes:
+  handleChange = (event,isColoRow) => {
+  const { value } = event.target;
+
+  // como es r, modificame este valor:
+    if (isColoRow ==="r"){
+  this.setState({ ...this.state, valueRow: value });
+//aqui arriba ya tengo el valor del input range
+}
+//como es c modifica este otro value:
+    else if(isColoRow==="c"){
+  
+  this.setState({ ...this.state, valueCol: value });
+}}
+    
 
   handleSubmit = (e) => {
 
       let arrayRows = [];
-        for (var rows = 0; rows < this.state.value; rows++){
+        for (var rows = 0; rows < this.state.valueRow; rows++){
           arrayRows[rows] = []
         
-          for (var columns = 0; columns < 5; columns++){
+          for (var columns = 0; columns < this.state.valueCol; columns++){
                 arrayRows[rows][columns] = false
             }
         }
@@ -32,15 +43,9 @@ export default class EditTables extends Component {
       let newArray = [...this.state.arrayRows]
       newArray[rowIndex][colIndex] =  !newArray[rowIndex][colIndex];
 
-      this.setState({...this.state, arrayRows: newArray})
-  
-    
+      this.setState({...this.state, arrayRows: newArray})  
     
   } 
-
-   
-
-  
   
   render() {
    
@@ -51,14 +56,26 @@ export default class EditTables extends Component {
           <fieldset>
             <label>FILAS</label>
             <input
-              value={this.state.value}
+              value={this.state.valueRow}
               type="range"
               min="1"
               max="10"
-              onChange={e => this.handleChange(e)}
+              onChange={e => this.handleChange(e,"r")}
             />
+            <h2>{this.state.valueRow}</h2>
 
-            <h2>{this.state.value}</h2>
+            <label>COLUMNAS</label>
+            <input
+              value={this.state.valueCol}
+              type="range"
+              min="1"
+              max="10"
+              //le paso abajo la c para diferenciar en la funcion las col de las rows
+              onChange={e => this.handleChange(e, "c")}
+            />
+           <h2>{this.state.valueCol}</h2>
+
+            
             <button onClick={this.handleSubmit}>Generate!</button>
           </fieldset>
 
