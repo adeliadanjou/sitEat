5bd9ca6378e6db9cf398fd7380b6536fb39a89e7
Adelia d Anjou <princessadelia@gmail.com>
2018-12-19 22:01:54 +0100

    cambio de portada home

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -16,10 +16,10 @@ class Home extends Component {
         <div>
            <div className="divHome">
         <img className="logoImage" src={require("../../sitEatLogo.png")} alt=""/>
-        <h1>¿Cansado de esperar al camarero? {this.props.username}</h1>
+        <h1 className="separate">¿Cansado de esperar al camarero? {this.props.username}</h1>
         <h3>¡Nunca más!</h3>
         <Link to={`/signup/`}><button className="btn2 btnWhite">REGÍSTRATE</button></Link>
-        <div className="alreadyUser"><p className="aBitSpace">¿Ya tienes cuenta?</p><Link to="/login">Iniciar sesión</Link></div>
+        <div className="alreadyUser"><p className="aBitSpace">¿Ya tienes cuenta?</p><Link className="login" to="/login">Iniciar sesión</Link></div>
        <img className="appStore" src={require("./playStore.png")} alt=""/>
           </div>
           <div className="section2">chungadas varias</div>
