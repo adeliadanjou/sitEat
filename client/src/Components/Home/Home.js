0b9d5c79dd7c8cafa5b5688905375795b4ddc280
Adelia d Anjou <princessadelia@gmail.com>
2018-12-19 22:07:08 +0100

    section 2 home done

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -14,15 +14,15 @@ class Home extends Component {
       
     return (
         <div>
-           <div className="divHome">
+           <section className="divHome">
         <img className="logoImage" src={require("../../sitEatLogo.png")} alt=""/>
         <h1 className="separate">¿Cansado de esperar al camarero? {this.props.username}</h1>
         <h3>¡Nunca más!</h3>
         <Link to={`/signup/`}><button className="btn2 btnWhite">REGÍSTRATE</button></Link>
         <div className="alreadyUser"><p className="aBitSpace">¿Ya tienes cuenta?</p><Link className="login" to="/login">Iniciar sesión</Link></div>
        <img className="appStore" src={require("./playStore.png")} alt=""/>
-          </div>
-          <div className="section2">chungadas varias</div>
+          </section>
+          
         </div>
         
      )
