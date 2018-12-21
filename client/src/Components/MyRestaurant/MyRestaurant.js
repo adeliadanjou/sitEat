ccaff06389da96702a9a40d4a33ef1f1d748708a
Adelia d Anjou <princessadelia@gmail.com>
2018-12-21 10:57:32 +0100

    mas maquetacion

--- client/src/Components/MyRestaurant/MyRestaurant.js
+++ client/src/Components/MyRestaurant/MyRestaurant.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import AuthService from "../../auth/AuthService";
+import './MyRestaurant.css';
 
 export default class MyRestaurant extends Component {
   constructor(props) {
@@ -17,20 +18,25 @@ export default class MyRestaurant extends Component {
   render() {
     console.log(this.state.misMesasHere);
     return (
-      <div>
+      <div className="searchImg">
         
+        <h1 className="perfilDe per">MIS MESAS:</h1>
         {this.state.misMesasHere
           ? this.state.misMesasHere.user.map((pedido, index) => {
               console.log(pedido);
               return (
                 //esto hay que hacerlo bien, pero solo es pintar!!
-                <div key={pedido._id}>
-                 <p>{pedido.name}</p>
-                 <p>{pedido.mesa}</p>
-
-                  <h3>{pedido.chairs}</h3>
-                  <h4>{pedido.pedido}</h4>
-                </div>
+                <div className="row">
+                <div  className="cardDiv" key={pedido._id}>
+                 <h1>Mesa: {pedido.mesa}</h1>
+                 <h6> Nombre: {pedido.name}</h6>
+                 <h6>{pedido.chairs} Personas</h6>
+                 <p>{pedido.pedido}</p>
+       
+                 </div>
+               
+             
+             </div>
               );
             })
           : ""}
