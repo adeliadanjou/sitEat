ed7b2a38d69e3ce207f744f9384ed1d5386c7582
Adelia d Anjou <princessadelia@gmail.com>
2019-01-15 00:25:24 +0100

    more update

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -18,7 +18,7 @@ class Home extends Component {
         <img  src={require("../../images/sitEatLogo.png")} alt=""/>
         <h1 className="separate tiredOfWait">¿Cansado de esperar al camarero? {this.props.username}</h1>
         <h3 >¡Nunca más!</h3>
-        <Link to={`/signup/`}><button className="btn2 btnWhite">REGÍSTRATE</button></Link>
+        <Link to={`/signup/`}><button className="btn2 btnWhite">REGISTRATE</button></Link>
         <div className="alreadyUser"><p className="aBitSpace">¿Ya tienes cuenta?</p><Link className="login" to="/login">Iniciar sesión</Link></div>
         <div className="sideBotoms">
         <img className="appStore" src={require("../../images/apple.png")} alt=""/>
