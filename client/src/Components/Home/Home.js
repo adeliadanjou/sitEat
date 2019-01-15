c693adae3abe2a2646005083b671484ec8fcbbb2
Adelia d Anjou <princessadelia@gmail.com>
2019-01-15 01:20:17 +0100

    more update

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -18,13 +18,30 @@ class Home extends Component {
         <img  src={require("../../images/sitEatLogo.png")} alt=""/>
         <h1 className="separate tiredOfWait">¿Cansado de esperar al camarero? {this.props.username}</h1>
         <h3 >¡Nunca más!</h3>
-        <Link to={`/signup/`}><button className="btn2 btnWhite">REGISTRATE</button></Link>
-        <div className="alreadyUser"><p className="aBitSpace">¿Ya tienes cuenta?</p><Link className="login" to="/login">Iniciar sesión</Link></div>
-        <div className="sideBotoms">
-        <img className="appStore" src={require("../../images/apple.png")} alt=""/>
-       <img className="appStore" src={require("../../images/google.png")} alt=""/>
+
+
+      <Link to={`/signup/`}>
+        <button className="grow btn2 ">REGISTRATE</button>
+        </Link>
+     
+       
+        
+        
+        <div className="alreadyUser">
+        <p className="alreadyAccount">¿Ya tienes cuenta?</p>
+        <Link className="login" to="/login">Iniciar sesión</Link>
+        </div>
+       
+       
+        <div className="sideButons">
+           <img className="appStore" src={require("../../images/apple.png")} alt=""/>
+           <img className="appStore" src={require("../../images/google.png")} alt=""/>
         </div>
    
+          </section>
+          <section className="section2">
+
+
           </section>
           
         </div>
